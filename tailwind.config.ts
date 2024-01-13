88759ffbcb83cf26a568f48ee3b237a4aed0f620
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-01-13 13:04:25 +0200

    feat(tailwind-custom-color): primary and secondary violet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary_violet: "#77216f",
+        secondary_violet: "#2c001e",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
